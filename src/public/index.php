b32f931b363e975a2a7c3896d0e34046f447ac0c
Nofrets Poai <opetstudio@gmail.com>
2021-07-09 10:06:20 +0700

    init

---
+++ src/public/index.php
@@ -0,0 +1,31 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'user';
+$config['db']['pass']   = 'password';
+$config['db']['dbname'] = 'exampleapp';
+
+// $app = new \Slim\App;
+$app = new \Slim\App(['settings' => $config]);
+$container = $app->getContainer();
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    // $this->logger->addInfo('Something interesting happened');
+
+    return $response;
+});
+$app->run();
